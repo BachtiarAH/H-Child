a43bfc397e25b190f4b8e51b596b7e6d2713dc62
BachtiarAH <bachtiarah73@gmail.com>
2022-12-15 08:24:43 +0700

    eror load cek kondisi

--- src/polije/views/home.java
+++ src/polije/views/home.java
@@ -127,6 +127,11 @@ public class home extends javax.swing.JFrame {
         );
 
         panelIdentitasPasien1.setBackground(new java.awt.Color(66, 83, 109));
+        panelIdentitasPasien1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                panelIdentitasPasien1MouseClicked(evt);
+            }
+        });
 
         logoIdentitasPasien1.setIcon(new javax.swing.ImageIcon(new javax.swing.ImageIcon(getClass().getResource("/polije/assets/Cek kondisi.png")).getImage().getScaledInstance(155, 155, Image.SCALE_DEFAULT)));
         logoIdentitasPasien1.setMaximumSize(new java.awt.Dimension(155, 155));
@@ -317,6 +322,13 @@ public class home extends javax.swing.JFrame {
         new Gizi().setVisible(true);
         this.dispose();
     }//GEN-LAST:event_panelIdentitasPasien2MouseClicked
+
+    private void panelIdentitasPasien1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_panelIdentitasPasien1MouseClicked
+        // TODO add your handling code here:
+        this.setVisible(false);
+        new CekKondisi().setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_panelIdentitasPasien1MouseClicked
 
     /**
      * @param args the command line arguments
